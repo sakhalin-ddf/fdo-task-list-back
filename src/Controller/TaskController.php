0ce3d9412848c497caf65442808023a0c189ae1b
sakhalin <73.vorobyev@gmail.com>
2022-01-15 06:15:18 +0300

    Remove .idea from git

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Service\Attribute\Required;
+
+#[Route(
+    path: '/api'
+)]
+class TaskController extends AbstractController
+{
+    #[Required]
+    public TaskRepository $repository;
+
+    #[Required]
+    public EntityManagerInterface $em;
+
+    #[Route(
+        path: '/task',
+        name: 'get-api-task',
+        methods: ['GET']
+    )]
+    public function list(): JsonResponse
+    {
+        return $this->json([
+            'status' => 'ok',
+            'data' => $this->repository->findAll(),
+        ]);
+    }
+
+    #[Route(
+        path: '/task',
+        name: 'post-api-task',
+        methods: ['POST']
+    )]
+    public function create(Request $request): JsonResponse
+    {
+        $task = new Task();
+
+        $task->setTitle($request->request->get('title'));
+        $task->setText($request->request->get('text'));
+
+        $this->em->persist($task);
+        $this->em->flush();
+
+        return $this->json([
+            'status' => 'ok',
+            'data' => $task,
+        ]);
+    }
+}
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -4,19 +4,25 @@ namespace App\Entity;
 
 use App\Repository\TaskRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
-#[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ORM\Entity(
+    repositoryClass: TaskRepository::class
+)]
 class Task
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[SerializedName('id')]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[SerializedName('title')]
     private $title;
 
     #[ORM\Column(type: 'text')]
+    #[SerializedName('text')]
     private $text;
 
     public function getId(): ?int
